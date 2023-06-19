74bb329effc2090983fb3010ae1f1e8d878d1748
duongCute123 <you@example.com>
2023-06-19 17:38:07 +0700

    tam i

---
+++ src/main/java/com/example/demo/controller/ProductController.java
@@ -0,0 +1,51 @@
+package com.example.demo.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.example.demo.entity.Product;
+import com.example.demo.service.ProductService;
+
+@RestController
+@RequestMapping("/api/v1")
+public class ProductController {
+	@Autowired
+	ProductService productService;
+
+	@GetMapping("")
+	public String getHello() {
+		return "Hello product";
+	}
+
+	@GetMapping("/product")
+	public List<Product> getAllProduct() {
+		List<Product> list = productService.findAllproduct();
+		return list;
+	}
+
+	@GetMapping("/product/{id}")
+	public Product getById(@PathVariable int id) {
+		return productService.getById(id);
+
+	}
+
+	@PostMapping("/product")
+	public Product themProduct(@RequestBody Product product) {
+		productService.saveProduct(product);
+		return product;
+	}
+
+	@DeleteMapping("/product")
+	public String xoaProduct(@PathVariable int id) {
+		productService.xoaProduct(id);
+		return "Xoá tahnffh công product";
+	}
+}
